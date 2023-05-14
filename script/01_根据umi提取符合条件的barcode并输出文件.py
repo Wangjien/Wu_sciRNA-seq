cc1475cf76062bcd24def9b86b28561b562b8995
Wangjien <52444578+Wangjien@users.noreply.github.com>
2023-05-14 22:56:42 +0800

    new

--- script/01_根据umi提取符合条件的barcode并输出文件.py
+++ script/01_根据umi提取符合条件的barcode并输出文件.py
@@ -6,7 +6,7 @@ import pandas as pd
 import pyfastx
 from multiprocessing import pool
 
-def Extract_barcode(input_folder, output_folder, sample_id, ligation_barcode_file, RT_Barcode_file, p7_file,log_file):
+def Extract_barcode(input_folder, output_folder, sample_id, ligation_barcode_file, RT_Barcode_file, p7_file):
     """
     从Read1序列中根据umi序列的位置,提取RT barcode和ligation barcode,添加到对应的双端测序的Read2序列,并输出结果文件。
     """
@@ -15,16 +15,19 @@ def Extract_barcode(input_folder, output_folder, sample_id, ligation_barcode_fil
     f2 = os.path.join(input_folder,f'{sample_id}_R2.fq.gz')
     f3 = os.path.join(output_folder,f"{sample_id}_R2_barcode.fq.gz")
     f3_w = gzip.open(f3, 'wb')
+    
     # 读入ligation barcode和RT barcode和p7的文件文件
     ligation_file = pd.read_csv(ligation_barcode_file, header=None, names=['ligation'],sep='\t')
     ligation_list = ligation_file['ligation'].tolist()
     RT_file = pd.read_csv(RT_Barcode_file, header=None, names=['RT_barcode'],sep='\t')
     RT_list = RT_file['RT_barcode'].to_list()
     p7 = pd.read_csv(p7_file, header=None, names=['sample','p7_barcode'],sep='\t')
-    p7_dict = dict(zip(p7[:,'sample'],p7[:,'p7_barcode']))f1 = os.path.join(input_folder,f'{sample_id}_R1.fq.gz')
+    p7_dict = dict(zip(p7[:,'sample'],p7[:,'p7_barcode']))
     f2 = os.path.join(input_folder,f'{sample_id}_R2.fq.gz')
     f3 = os.path.join(output_folder,f"{sample_id}_R2_barcode.fq.gz")
     f3_w = gzip.open(f3, 'wb')
+    log = open(os.path.join(output_folder,f"{sample_id}_log.txt"),'w')
+    
     # 读入ligation barcode和RT barcode和p7的文件文件
     ligation_file = pd.read_csv(ligation_barcode_file, header=None, names=['ligation'],sep='\t')
     ligation_list = ligation_file['ligation'].tolist()
@@ -32,6 +35,7 @@ def Extract_barcode(input_folder, output_folder, sample_id, ligation_barcode_fil
     RT_list = RT_file['RT_barcode'].to_list()
     p7 = pd.read_csv(p7_file, header=None, names=['sample','p7_barcode'],sep='\t')
     p7_dict = dict(zip(p7['sample'].tolist(),p7['p7_barcode'].tolist()))
+    
     # 接受符合条件的序列
     ligation_match = []
     RT_match=[]
@@ -51,7 +55,9 @@ def Extract_barcode(input_folder, output_folder, sample_id, ligation_barcode_fil
     merge_barcode_umi = {k:v1+v2+str(p7_dict[sample_id])+','+umi_seq for k,v1,v2, umi_seq in zip(name_match,RT_match,ligation_match,umi_match)}
     # 从Read2序列中查找符合的条件
     count = 0
+    reads_count = []
     for name,seq,qual in pyfastx.Fastq(f2,build_index=False):  
+        reads_count.append(name)
         if name in merge_barcode_umi:
             # print('@' +name + ',' +  merge_barcode_umi[name])
             # print(seq)
@@ -63,12 +69,19 @@ def Extract_barcode(input_folder, output_folder, sample_id, ligation_barcode_fil
             f3_w.write((qual + '\n').encode)
             count +=1
             percent = (count/len(merge_barcode_umi))*100
-            print('\r'+">>>>>>>>>>%d%% (%d/%d)" %(percent,count,len(merge_barcode_umi)),end="")
+            print('\r' + ">>>>>>>>>>>>>>>>>>>>>>> %d%% (%d/%d)" %(percent,count,len(merge_barcode_umi)),end="")
             # print(count)
-
-
-
-
+    time.end = time.time()
+    log.write(
+        """
+        测序总reads数目:%s,
+        符合条件的reads数目:%s,
+        占比:%.4f,
+        耗时:%s 秒
+        """ % (len(reads_count),len(merge_barcode_umi),
+               len(reads_count)/len(merge_barcode_umi),
+               time.end - time.start))
+    
 if __name__ == '__main__':
     input_folder = '/home/data/vip9t22/test/BC230502-1'
     output_folder = '/home/data/vip9t22/test/BC230502-1'
