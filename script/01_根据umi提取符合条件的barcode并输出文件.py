67845ceaae828a5355e57aac07523990d3a4dfa3
Wangjien <52444578+Wangjien@users.noreply.github.com>
2023-05-14 21:51:56 +0800

    new

--- script/01_根据umi提取符合条件的barcode并输出文件.py
+++ script/01_根据umi提取符合条件的barcode并输出文件.py
@@ -21,25 +21,53 @@ def Extract_barcode(input_folder, output_folder, sample_id, ligation_barcode_fil
     RT_file = pd.read_csv(RT_Barcode_file, header=None, names=['RT_barcode'],sep='\t')
     RT_list = RT_file['RT_barcode'].to_list()
     p7 = pd.read_csv(p7_file, header=None, names=['sample','p7_barcode'],sep='\t')
-    p7_dict = dict(zip(p7[:,'sample'],p7[:,'p7_barcode']))
+    p7_dict = dict(zip(p7[:,'sample'],p7[:,'p7_barcode']))f1 = os.path.join(input_folder,f'{sample_id}_R1.fq.gz')
+    f2 = os.path.join(input_folder,f'{sample_id}_R2.fq.gz')
+    f3 = os.path.join(output_folder,f"{sample_id}_R2_barcode.fq.gz")
+    f3_w = gzip.open(f3, 'wb')
+    # 读入ligation barcode和RT barcode和p7的文件文件
+    ligation_file = pd.read_csv(ligation_barcode_file, header=None, names=['ligation'],sep='\t')
+    ligation_list = ligation_file['ligation'].tolist()
+    RT_file = pd.read_csv(RT_Barcode_file, header=None, names=['RT_barcode'],sep='\t')
+    RT_list = RT_file['RT_barcode'].to_list()
+    p7 = pd.read_csv(p7_file, header=None, names=['sample','p7_barcode'],sep='\t')
+    p7_dict = dict(zip(p7['sample'].tolist(),p7['p7_barcode'].tolist()))
     # 接受符合条件的序列
     ligation_match = []
     RT_match=[]
     name_match = []
     umi_match = []
-    for name,seq,qual in pyfastx.Fastq(f1, build_index=False)
+    for name,seq,qual in pyfastx.Fastq(f1, build_index=False):
         if 'CAGAGC' in seq:
             umi_index = seq.find('CAGAGC')
-            match_RT = seq[int(umi_index -10):int(umi_index -1)]
-            match_ligation = seq[int(umi_index + 6+8):int(umi_index+6+18)]
+            match_ligation = seq[int(umi_index -10):int(umi_index -1)]
+            match_RT = seq[int(umi_index + 6+8):int(umi_index+6+18)]
             if match_RT in RT_list and match_ligation in ligation_list:
                 ligation_match.append(match_ligation)
                 RT_match.append(match_RT)
                 umi = seq[int(umi_index):int(umi_index+14)]
                 name_match.append(name)
-    merge_barcode_umi = {k:v1+v2+p7[sample_id]+','+umi for k,v1,v2, umi_seq in zip(name_match,RT_match,ligation_match,umi_match)}
-    
+                umi_match.append(umi)
+    merge_barcode_umi = {k:v1+v2+str(p7_dict[sample_id])+','+umi_seq for k,v1,v2, umi_seq in zip(name_match,RT_match,ligation_match,umi_match)}
+    # 从Read2序列中查找符合的条件
+    count = 0
+    for name,seq,qual in pyfastx.Fastq(f2,build_index=False):  
+        if name in merge_barcode_umi:
+            # print('@' +name + ',' +  merge_barcode_umi[name])
+            # print(seq)
+            # print('+')
+            # print(qual)
+            count +=1
+            print(count)
 
+
+if __name__ == '__main__':
+    input_folder = '/home/data/vip9t22/test/BC230502-1'
+    output_folder = '/home/data/vip9t22/test/BC230502-1'
+    sample_id = 'BC230502-1'
+    ligation_barcode_file = '/home/data/vip9t22/test/BC230502-1/ligation_barcode.txt'
+    RT_Barcode_file = '/home/data/vip9t22/test/BC230502-1/RT_barcode.txt'
+    p7_file = '/home/data/vip9t22/test/BC230502-1/sample_p7.txt'
 
 
 
